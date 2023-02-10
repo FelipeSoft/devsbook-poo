1d7a727ab18f1500dae58f1872bbe3574dc41e77
FelipeSoft <felipedecastrolima2@gmail.com>
2023-02-10 09:35:40 -0300

    adding files

---
+++ database.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Maio-2021 às 01:34
+-- Versão do servidor: 10.4.18-MariaDB
+-- versão do PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `devsbook`
+--
+CREATE DATABASE IF NOT EXISTS `devsbook` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `devsbook`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postcomments`
+--
+
+DROP TABLE IF EXISTS `postcomments`;
+CREATE TABLE `postcomments` (
+  `id` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postlikes`
+--
+
+DROP TABLE IF EXISTS `postlikes`;
+CREATE TABLE `postlikes` (
+  `id` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `userrelations`
+--
+
+DROP TABLE IF EXISTS `userrelations`;
+CREATE TABLE `userrelations` (
+  `id` int(11) NOT NULL,
+  `user_from` int(11) NOT NULL,
+  `user_to` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `birthdate` date NOT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `work` varchar(100) DEFAULT NULL,
+  `avatar` varchar(100) NOT NULL DEFAULT 'default.jpg',
+  `cover` varchar(100) NOT NULL DEFAULT 'cover.jpg',
+  `token` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `postcomments`
+--
+ALTER TABLE `postcomments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `postlikes`
+--
+ALTER TABLE `postlikes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `userrelations`
+--
+ALTER TABLE `userrelations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `postcomments`
+--
+ALTER TABLE `postcomments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `postlikes`
+--
+ALTER TABLE `postlikes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `userrelations`
+--
+ALTER TABLE `userrelations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
